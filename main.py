c4bbe5bc74778480c2ed301e9c5c2aa9399298b5
Erick Murrieta <ercx80@gmail.com>
2019-10-08 00:17:22 -0400

    This is the final version

--- main.py
+++ main.py
@@ -18,30 +18,54 @@ class Blog(db.Model): #the class represents the data that will be stored in the 
         self.title = title
         self.body = body
 
-@app.route('/blog', methods=[ 'GET']) #this handler has a GET method because it sends a get request to the database. No data is posted.
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/blog', methods=[ 'GET']) #this handler has a GET method because it sends a get request to the database. No data is posted.
+def blog():
+    blog_id = request.args.get('id')
+    if blog_id == None:
+        blog = Blog.query.all() #this line will query the blog entries from the database
+        return render_template('main_blog.html', blog=blog) #This line renders the main blog page along with the posts.
+
+    else:
+        posts = Blog.query.get(blog_id)
+        return render_template('individual_entry.html', posts=posts)
 
     
-
     
-    blog = Blog.query.all() #this line will query the blog entries from the database
      
     
-    return render_template('main_blog.html', blog=blog) #This line renders the main blog page along with the posts.
+   
 
 @app.route('/new_post', methods=[ 'POST', 'GET'])#This route gest both get and post, because it posts to the database, and it gets the form to load. 
 def new_post():
     if request.method == 'POST':
-        blog_name = request.form['title']
+        blog_name = request.form['new']
         entry = request.form['blog']            #This if statement gets the data from the form and creates an object from the Blog class.
-        new_entry = Blog(blog_name, entry)
         
-        db.session.add(new_entry)  #These lines add and commit the data to the database. 
-        db.session.commit()
-        return redirect('/blog') #After adding the data to databse, it redirects back to the main blog page.
-    else:
-        return render_template('new_blog.html') 
+        name_error = ""
+        body_error = ""
+        if not blog_name:
+            name_error = "This field cannot be empty"
+        if not entry:
+            body_error = "This field cannot be empty"
+        if not name_error and not body_error:
+            new_entry = Blog(blog_name, entry)
 
+        
+            db.session.add(new_entry)  #These lines add and commit the data to the database. 
+            db.session.commit()
+            return redirect('/blog?id={}'.format(new_entry.id)) #After adding the data to databse, it redirects back to the main blog page.
+        else:
+            return render_template('new_blog.html',name_error=name_error,body_error=body_error,blog_name=blog_name,entry=entry) 
+        
+    return render_template('new_blog.html')
+
+    
+
+ 
 
 
 
